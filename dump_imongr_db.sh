02b455fb4a1fe3da27fc3db7624ffda0c066ba73
Are Edvardsen <biorakel@gmail.com>
2020-07-24 11:08:55 +0200

    no need for tarball

--- dump_imongr_db.sh
+++ dump_imongr_db.sh
@@ -2,5 +2,5 @@
 
 docker exec -it mariadb sh -c 'exec mysqldump --add-drop-database --skip-comments --compact --single-transaction -u$MYSQL_USER -p"$MYSQL_PASSWORD" --databases "$MYSQL_DATABASE"' > imongr_db_dump.sql
 
-tar czf imongr_db_dump.sql.tar.gz imongr_db_dump.sql
+gzip imongr_db_dump.sql
 
