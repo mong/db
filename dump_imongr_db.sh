cc4b14ce3c210224e0b3dcde183152392216bcc2
Are Edvardsen <biorakel@gmail.com>
2020-07-24 09:23:30 +0200

    extending

--- dump_imongr_db.sh
+++ dump_imongr_db.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker exec -it mariadb sh -c 'exec mysqldump -u$MYSQL_USER -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE"' > imongr_db_dump.sql
+docker exec -it mariadb sh -c 'exec mysqldump -add-drop-database --skip-comments --compact --single-transaction -u$MYSQL_USER -p"$MYSQL_PASSWORD" --databases "$MYSQL_DATABASE"' > imongr_db_dump.sql
+
+tar czf imongr_db_dump.sql.tar.gz imongr_db_dump.sql
+
